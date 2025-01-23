598325d14881569738572a79e1c026efa03110c1
Maks-Nata <mixcim20121984@gmail.com>
2025-01-23 13:42:10 +0200

    changed api.servisec.tsx

--- src/servisec/api.servisec.tsx
+++ src/servisec/api.servisec.tsx
@@ -1,7 +1,9 @@
-const  baceURL='https://dummyjson.com'
-console.log(baceURL)
-export const getUsers = async <T>(): Promise<T> => {
-    return    await fetch(`${baceURL}/users`).then(res => res.json())
+import {IUserResponseModel} from "../modules/IUserResponseModel.tsx";
 
-
+const baceUrl = 'https://dummyjson.com'
+export const UserUrl = {
+    getAllUsers: async (): Promise<IUserResponseModel> => {
+        return await fetch(baceUrl + '/users')
+            .then(value => value.json())
+    }
 }
